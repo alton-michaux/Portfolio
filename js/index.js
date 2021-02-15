f409fc77098472f333d92a77aefc977dd8afb4b6
Alton Michaux <alton.michaux@yahoo.com>
2021-02-15 18:58:04 -0500

    added some initial javascript for dropdown menu and added a disabled selected option to menu for placeholder purposes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
+const uiController = (function () {
+  const domElements = {
+    dropdown: "#projects",
+  };
 
+  //   console.log("ui online");
+  return {
+    domOutputs() {
+      return {
+        dropdown: document.querySelector(domElements.dropdown),
+      };
+    },
+  };
+})();
+
+const pageFunctions = (function (uiCtrl) {
+  //create a variable to select dom elements from ui controller
+  const domSelector = uiCtrl.domOutputs();
+  //select dropdown element
+  const dropdownMenu = domSelector.dropdown;
+  //   console.log(dropdownMenu);
+  dropdownMenu.addEventListener("change", () => {
+    const dropdownValue = dropdownMenu.options[dropdownMenu.selectedIndex].value;
+    // console.log(dropdownValue)
+    if (dropdownValue == "Apps") {
+        console.log("You picked Apps!");
+    } else {
+        console.log("You picked Games!");
+    }
+  });
+})(uiController);
