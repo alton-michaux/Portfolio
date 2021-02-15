449dd8b8dddf891cf5de48c187e1dfd3e9c2f02f
Alton Michaux <alton.michaux@yahoo.com>
2021-02-14 19:55:42 -0500

    working on some mobile optimizations, also trying to find out why links are unresponsive

--- js/index.js
+++ js/index.js
@@ -1,15 +1,1 @@
-const projects = {
-  apps: ["weather-app/index.html", "rsvp-app/index.html"],
-  games: ["games/breakout.html", "games/battleship.html"],
-};
 
-const projectOptions = document
-  .getElementById("projects")
-  .addEventListener("click", () => {
-      for (i = 0; i < projects.length; i++) {
-          console.log(projects.length)
-        const projectOptions = document.createElement('option')
-            projectOptions.innerHTML = projects[i];
-      }
-    ;
-  });
