5e46ef2a6936a596a5d711248a002dc9f81dd243
Alton Michaux <alton.michaux@yahoo.com>
2021-02-15 19:21:55 -0500

    added two canvases that will show the selected group of projects for selection

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
+//create a function to handle dom element selection
 const uiController = (function () {
   const domElements = {
     dropdown: "#projects",
+    appCanvas: "#app-canvas",
+    gameCanvas: "#game-canvas",
   };
 
   //   console.log("ui online");
+  //return a method that selects elements
   return {
     domOutputs() {
       return {
         dropdown: document.querySelector(domElements.dropdown),
+        appCanvas: document.querySelector(domElements.appCanvas),
+        gameCanvas: document.querySelector(domElements.gameCanvas),
       };
     },
   };
@@ -18,14 +24,20 @@ const pageFunctions = (function (uiCtrl) {
   const domSelector = uiCtrl.domOutputs();
   //select dropdown element
   const dropdownMenu = domSelector.dropdown;
-  //   console.log(dropdownMenu);
+  const appCanvas = domSelector.appCanvas;
+  const gameCanvas = domSelector.gameCanvas;
+    console.log(gameCanvas);
   dropdownMenu.addEventListener("change", () => {
     const dropdownValue = dropdownMenu.options[dropdownMenu.selectedIndex].value;
     // console.log(dropdownValue)
     if (dropdownValue == "Apps") {
         console.log("You picked Apps!");
+        appCanvas.style.display = "block";
+        gameCanvas.style.display = "none";
     } else {
         console.log("You picked Games!");
+        gameCanvas.style.display = "block";
+        appCanvas.style.display = "none";
     }
   });
 })(uiController);
