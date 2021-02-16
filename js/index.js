e0e2a9b01748d546257f3e9249f9b007e028d3fa
Alton Michaux <alton.michaux@yahoo.com>
2021-02-16 10:42:11 -0500

    still working on project display, got weather commits done

--- js/index.js
+++ js/index.js
@@ -11,9 +11,9 @@ const projectInfo = (function () {
   const imagePath = {
     spotify: "../images/spotify.png",
     weather: "../images/weather.png",
-    rsvp: "../images/Rsvp",
+    rsvp: "../images/Rsvp.png",
     breakout: "../images/Breakout.png",
-    battleship: "../images/Battleship.png",
+    battleship: "../images/battleship.png",
   };
 
   return {
@@ -50,8 +50,9 @@ const projectInfo = (function () {
 const uiController = (function () {
   const domElements = {
     dropdown: "#projects",
-    appCanvas: "#app-canvas",
-    gameCanvas: "#game-canvas",
+    projectDisplay: ".project-display",
+    appDiv: "#app-div",
+    gameDiv: "#game-div",
   };
 
   //   console.log("ui online");
@@ -60,26 +61,32 @@ const uiController = (function () {
     domOutputs() {
       return {
         dropdown: document.querySelector(domElements.dropdown),
-        appCanvas: document.querySelector(domElements.appCanvas),
-        gameCanvas: document.querySelector(domElements.gameCanvas),
+        projectDisplay: document.querySelector(domElements.projectDisplay),
+        appDiv: document.querySelector(domElements.appDiv),
+        gameDiv: document.querySelector(domElements.gameDiv),
       };
     },
 
-    placeOnAppGrid(image, projectPath) {
-      const html = `<button class="project-blocks"><img src=${image} alt=${projectPath}/>${projectPath}</button>`;
+    placeOnAppGrid(image, filePath) {
+      const html = `<a class="project-link" href="${filePath}"><canvas class="project-blocks" id="apps"><img class="thumbnail" src=${image} alt=${filePath}/></canvas></a>`;
       document
-        .querySelector(domElements.appCanvas)
+        .querySelector(domElements.appDiv)
         .insertAdjacentHTML("beforeend", html);
     },
 
-    placeOnGameGrid(image, projectPath) {
-        const html = `<button class="project-blocks"><img src=${image} alt=${projectPath}/>${projectPath}</button>`;
-        document
-          .querySelector(domElements.gameCanvas)
-          .insertAdjacentHTML("beforeend", html);
-      },
-  };
-})();
+    placeOnGameGrid(image, filePath) {
+      const html = `<a class="project-link" href="${filePath}"><canvas class="project-blocks" id="games"><img class="thumbnail" src=${image} alt=${filePath}/></canvas></a>`;
+      document
+        .querySelector(domElements.gameDiv)
+        .insertAdjacentHTML("beforeend", html);
+    },
+
+    resetCanvas() {
+      domElements.projectDisplay.innerHTML = "";
+      console.log('reset!');
+    },
+    }
+  })();
 
 const pageFunctions = (function (proInfo, uiCtrl) {
   //----Information selectors----//
@@ -91,39 +98,50 @@ const pageFunctions = (function (proInfo, uiCtrl) {
   const gameImageSelector = proInfo.gameImageSelectors();
 
   //---DOM selectors---//
+
   //create a variable to select dom elements from ui controller
   const domSelector = uiCtrl.domOutputs();
   //select dropdown element
   const dropdownMenu = domSelector.dropdown;
-  const appCanvas = domSelector.appCanvas;
-  const gameCanvas = domSelector.gameCanvas;
-  console.log(appSelector, appImageSelector, gameSelector, gameImageSelector);
-
+  const appDiv = domSelector.appDiv;
+  const gameDiv = domSelector.gameDiv;
+  //console.log(appSelector, appImageSelector, gameSelector, gameImageSelector);
   //----Event Listener----//
   dropdownMenu.addEventListener("change", () => {
     const dropdownValue =
       dropdownMenu.options[dropdownMenu.selectedIndex].value;
-    // console.log(dropdownValue)
     if (dropdownValue == "Apps") {
+      uiCtrl.resetCanvas();
       console.log("You picked Apps!");
-      appCanvas.style.border = "1px solid rgb(13, 48, 122)";
-      appCanvas.style.display = "block";
-      gameCanvas.style.display = "none";
+      appDiv.style.border = "1px solid rgb(13, 48, 122)";
+      appDiv.style.display = "grid";
+      gameDiv.style.display = "none";
       for (const app in appSelector) {
-          for (const image in appImageSelector) {
-              uiCtrl.placeOnAppGrid(image, app);
+        for (const image in appImageSelector) {
+          uiCtrl.placeOnAppGrid(image, app);
+          let appContext = document.querySelector("#apps").getContext("2d");
+          // console.log(appContext)
+          image.onload = () => {
+            appContext.drawImage(image, 20, 10, 1300, 1300, 0, 0, 300, 336)
           }
+        }
       }
     } else {
+      uiCtrl.resetCanvas();
       console.log("You picked Games!");
-      gameCanvas.style.border = "1px solid rgb(46, 65, 105)";
-      gameCanvas.style.display = "block";
-      appCanvas.style.display = "none";
+      gameDiv.style.border = "1px solid rgb(46, 65, 105)";
+      gameDiv.style.display = "grid";
+      appDiv.style.display = "none";
       for (const game in gameSelector) {
         for (const image in gameImageSelector) {
-            uiCtrl.placeOnGameGrid(image, game);
+          uiCtrl.placeOnGameGrid(image, game);
+          let gameContext = document.querySelector("#games").getContext("2d");
+          // console.log(appContext)
+          image.onload = () => {
+            gameContext.drawImage(image, 20, 10, 1300, 1300, 0, 0, 300, 336)
+          }
         }
-    }
+      }
     }
   });
 })(projectInfo, uiController);
