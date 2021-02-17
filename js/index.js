3f2ba150812f225f0d83a5d8004e42448caf8b00
Alton Michaux <alton.michaux@yahoo.com>
2021-02-17 10:39:12 -0500

    decided to go with manual placement of images for sake of time and styled buttons, js is only used to hide and switch divs based on dropdown selection

--- js/index.js
+++ js/index.js
@@ -1,27 +1,3 @@
-const spotifyImg = new Image()
-spotifyImg.src = "images/spotify.png"
-spotifyImg.alt = "spotify playlist app"
-
-const weatherImg = new Image()
-weatherImg.src = "images/weather.jpg"
-weatherImg.alt = "weather app"
-
-const rsvpImg = new Image()
-rsvpImg.src = "images/Rsvp.png"
-rsvpImg.alt = "rsvp app"
-
-const breakoutImg = new Image()
-breakoutImg.src = "images/Breakout.png"
-breakoutImg.alt = "breakout game"
-
-const battleshipImg = new Image()
-battleshipImg.src = "images/battleship.png"
-battleshipImg.alt = "battleship game"
-
-const apps = [spotifyImg, weatherImg, rsvpImg];
-const games = [breakoutImg, battleshipImg];
-// console.log(apps, games);
-
 //create a controller to handle dom element selection
 const uiController = (function () {
   const domElements = {
@@ -41,30 +17,6 @@ const uiController = (function () {
         appDiv: document.querySelector(domElements.appDiv),
         gameDiv: document.querySelector(domElements.gameDiv),
       };
-    },
-
-    placeAppImages(arr) {
-      for (i = 0; i < arr.length; i++) {
-        let appCanvas = document.getElementsByClassName("project-blocks")[i];
-        let appContext = appCanvas.getContext("2d");
-        console.log(appContext)
-        arr[i].onload = () => {
-          appContext.drawImage(arr[i], 20, 10, 300, 300, 0, 0, 200, 200);
-      }
-      };
-      console.log("placing image...");
-    },
-
-    placeGameImages(arr) {
-      for (i = 0; i < arr.length; i++) {
-        let gameCanvas = document.getElementsByClassName('project-blocks')[i]
-        let gameContext = gameCanvas.getContext("2d");
-        console.log(gameContext)
-        arr[i].onload = () => {
-          gameContext.drawImage(arr[i], 20, 10, 300, 300, 0, 0, 200, 200);
-      }
-      };
-      console.log("placing image...");
     },
 
     resetCanvas() {
@@ -93,13 +45,11 @@ const pageFunctions = (function (uiCtrl) {
       console.log("You picked Apps!");
       appDiv.style.display = "grid";
       gameDiv.style.display = "none";
-      uiCtrl.placeAppImages(apps);
     } else {
       uiCtrl.resetCanvas();
       console.log("You picked Games!");
       gameDiv.style.display = "grid";
       appDiv.style.display = "none";
-      uiCtrl.placeGameImages(games);
     }
   });
 })(uiController);
